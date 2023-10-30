bc1c71dae2ea478f2fafd5f84b42e1ed770cfc4a
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2023-10-30 13:14:38 +0900

    Create diff.ts

---
+++ diff.ts
@@ -0,0 +1,54 @@
+function diff(RDOM, VDOM) {
+    if (RDOM.isEqualNode(VDOM)) {
+        return;
+    }
+
+    if (RDOM.nodeType === Node.TEXT_NODE) {
+        if (RDOM.textContent !== VDOM.textContent) {
+            RDOM.textContent = VDOM.textContent;
+        }
+        return;
+    }
+
+    if (RDOM.tagName !== VDOM.tagName) {
+        RDOM.replaceWith(VDOM);
+        return;
+    }
+
+    const RAttrs = RDOM.attributes;
+    const VAttrs = VDOM.attributes;
+
+    for (let i = RAttrs.length - 1; i >= 0; i--) {
+        const attr = RAttrs[i];
+        const { name } = attr;
+        const vAttr = VAttrs.getNamedItem(name);
+        if (!vAttr) {
+            RDOM.removeAttribute(name);
+        } else if (attr.value !== vAttr.value) {
+            RDOM.setAttribute(name, vAttr.value);
+        }
+        VAttrs.removeNamedItem(name);
+    }
+
+    for (let i = 0; i < VAttrs.length; i++) {
+        const vAttr = VAttrs[i];
+        const { name, value } = vAttr;
+        RDOM.setAttribute(name, value);
+    }
+
+    const RChildren = RDOM.childNodes;
+    const VChildren = VDOM.childNodes;
+
+    for (let i = RChildren.length - 1; i >= 0; i--) {
+        const RChild = RChildren[i];
+        if (!VChildren[i]) {
+            RChild.remove();
+        } else {
+            diff(RChild, VChildren[i]);
+        }
+    }
+
+    for (let i = RChildren.length; i < VChildren.length; i++) {
+        RDOM.appendChild(VChildren[i]);
+    }
+}
